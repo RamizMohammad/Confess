7bcee8fe0752812b89b0c7a9f7af96d5e6a4c5b9
Mohammad Ramiz <ramizanas6@gmail.com>
2025-05-28 18:05:51 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,42 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from flask import Flask,request,jsonify
+import json
+
+app = Flask(__name__)
+
+class serverClass:
+    def __init__(self):
+        try:
+            if not firebase_admin._apps:
+                cred = credentials.Certificate("confess-f706a-firebase-adminsdk-fbsvc-64580a51d4.json")
+                firebase_admin.initialize_app(cred)
+            self.db = firestore.client()
+            self.status = "Firestore Connected"
+        except:
+            self.status = "Firebase Not Connected"
+
+    def addData(self,data):
+        try:
+            self.db.collection("Confession-UserData").add(data)
+            self.status = "Data Saved Successfully"
+        except:
+            self.status = "Error in data saving"
+
+@app.route('/')
+def establishConnection():
+    return server.status
+
+@app.route('/add-data', methods=['POST'])
+def saveData():
+    res = request.get_json()
+    server.addData(res)
+    return server.status
+
+if __name__ == "__main__":
+    server = serverClass()
+    app.run(
+        port=5000,
+        debug=True
+    )
